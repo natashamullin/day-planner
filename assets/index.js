7176a1e82badc9626badb23d30e55fa17488a909
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2020-12-21 22:10:01 -0800

    local storage saves the info

--- assets/index.js
+++ assets/index.js
@@ -27,5 +27,25 @@ $(document).ready(function () {
     // run hour up dater
     hourUpdater();
 
+    //set up interval to update current time every minute
+    const interval = setInterval(hourUpdater, 60000);
+    //save on click
+    $(".saveBtn").on("click", function () {
+        const value = $(this).siblings(".textarea").val();
+        const time = $(this).parent().attr("id")
 
+        //save to local store so that it comes back up on reload
+        localStorage.setItem(time, value)
+    });
+
+    //load saved info
+    $("#9am .textarea").val(localStorage.getItem("9am"));
+    $("#10am .textarea").val(localStorage.getItem("10am"));
+    $("#11am .textarea").val(localStorage.getItem("11am"));
+    $("#12pm .textarea").val(localStorage.getItem("12am"));
+    $("#1pm .textarea").val(localStorage.getItem("1pm"));
+    $("#2pm .textarea").val(localStorage.getItem("2pm"));
+    $("#3pm .textarea").val(localStorage.getItem("3pm"));
+    $("#4pm .textarea").val(localStorage.getItem("4pm"));
+    $("#5pm .textarea").val(localStorage.getItem("5pm"));
 });
